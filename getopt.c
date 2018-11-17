1c8601c0f214eb4d690597e572213c33fa92442b
Richard James Howe <howe.r.j.89@gmail.com>
2018-11-17 12:58:24 +0000

    Added getopt.c, which will need integration
    A getopt implementation has been added, this will need integration with
    the pickle library.
    
    A minor annoyance with the 'make clean' option has been fixed.

---
+++ getopt.c
@@ -0,0 +1,73 @@
+#include <string.h>
+#include <stdio.h>
+
+/* From: <https://stackoverflow.com/questions/10404448/getopt-h-compiling-linux-c-code-in-windows> 
+ * @todo Integrate this, in a modified form, into the pickle library */
+
+int opterr = 1, /* if error message should be printed */
+    optind = 1, /* index into parent argv vector */
+    optopt,     /* character checked for validity */
+    optreset;   /* reset getopt */
+char *optarg;   /* argument associated with option */
+
+#define BADCH   (int)'?'
+#define BADARG  (int)':'
+#define EMSG    ""
+
+/* getopt -- Parse argc/argv argument vector. */
+int getopt(int nargc, char *const nargv[], const char *ostr)
+{
+	static char *place = EMSG; /* option letter processing */
+	const char *oli; /* option letter list index */
+
+	if (optreset || !*place) { /* update scanning pointer */
+		optreset = 0;
+		if (optind >= nargc || *(place = nargv[optind]) != '-') {
+			place = EMSG;
+			return -1;
+		}
+		if (place[1] && *++place == '-') { /* found "--" */
+			++optind;
+			place = EMSG;
+			return -1;
+		}
+	} /* option letter okay? */
+	if ((optopt = (int)*place++) == (int)':' ||
+	    !(oli = strchr(ostr, optopt))) {
+		/*
+		 * if the user didn't specify '-' as an option,
+		 * assume it means -1.
+		 */
+		if (optopt == (int)'-')
+			return -1;
+		if (!*place)
+			++optind;
+		if (opterr && *ostr != ':')
+			(void)printf("illegal option -- %c\n", optopt);
+		return BADCH;
+	}
+	if (*++oli != ':') { /* don't need argument */
+		optarg = NULL;
+		if (!*place)
+			++optind;
+	} else {  /* need an argument */
+		if (*place) /* no white space */
+			optarg = place;
+		else if (nargc <= ++optind) { /* no arg */
+			place = EMSG;
+			if (*ostr == ':')
+				return BADARG;
+			if (opterr)
+				(void)
+				    printf
+				    ("option requires an argument -- %c\n",
+				     optopt);
+			return BADCH;
+		} else		/* white space */
+			optarg = nargv[optind];
+		place = EMSG;
+		++optind;
+	}
+	return optopt; /* dump back option letter */
+}
+
